c9fde7b6bdcb38570b6396850a518f2ab2731a1a
Lucas <lgs.08lucas@gmail.com>
2024-08-12 14:40:37 -0300

    Criado contexto para usuário autenticado

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
 import "./App.css";
+
 import { BrowserRouter } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
+
+import { AuthProvider } from "./context/AuthContext";
+
 import BlogRoutes from "./BlogRoutes";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 function App() {
+  const [user, setUser] = useState(undefined);
+  const {auth} = useAuthentication();
+
+  const loadingUser = user === undefined;
+
+  useEffect(() =>{
+    onAuthStateChanged(auth, (user) => setUser(user)); 
+    //Se o usuario estiver autenticado, pega ela, caso contrário define como false
+  }, [auth])
+
+  if(loadingUser){
+    return <p>Carregando...</p>
+  }
+
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <div className="container">
-          <BlogRoutes />
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider value={{user}}>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <BlogRoutes />
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
