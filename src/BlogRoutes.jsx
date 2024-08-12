4c997ae8025c5c030c3294322338f71e66634211
Lucas <lgs.08lucas@gmail.com>
2024-08-12 15:11:20 -0300

    Adicionado bloqueio nas rotas e criado conteudo da página About

--- src/BlogRoutes.jsx
+++ src/BlogRoutes.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
 
+import { useAuthValue } from "./context/AuthContext";
+
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Login from "./pages/Login/Login";
@@ -9,14 +11,24 @@ import CreatePost from "./pages/CreatePost/CreatePost";
 import Dashboard from "./pages/Dashboard/Dashboard";
 
 const BlogRoutes = () => {
+	const { user } = useAuthValue();
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="/about" element={<About />} />
-			<Route path="/login" element={<Login />} />
-			<Route path="/register" element={<Register />} />
-			<Route path="/posts/create" element={<CreatePost />} />
-			<Route path="/dashboard" element={<Dashboard />} />
+			<Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+			<Route
+				path="/register"
+				element={!user ? <Register /> : <Navigate to="/" />}
+			/>
+			<Route
+				path="/posts/create"
+				element={user ? <CreatePost /> : <Navigate to="/login" />}
+			/>
+			<Route
+				path="/dashboard"
+				element={user ? <Dashboard /> : <Navigate to="/login" />}
+			/>
 		</Routes>
 	);
 };
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import styles from './About.module.css'
+import React from "react";
+import styles from "./About.module.css";
+import { Link } from "react-router-dom";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+	return (
+		<div className={styles.about}>
+			<h2>
+				Sobre o React<span>Blog</span>
+			</h2>
+			<p>
+				Este projeto consiste em um blog desenvolvido em React para o front-end
+				e Firebase para o back-end.
+			</p>
+			<p>
+				Ele foi desenvolvido com base no nos conhecimentos que eu obtive no
+				curso{" "}
+				<a href="https://www.udemy.com/course/react-do-zero-a-maestria-c-hooks-router-api-projetos/?couponCode=KEEPLEARNING">
+					React do Zero a Maestria (c/ hooks, router, API, Projetos)
+				</a>{" "}
+				sendo uma versão com alteraçoes do Mini<b>Blog</b> que foi criado no
+				curso.
+			</p>
+			<p>
+				Começe a utiliza-lo criando um post já!{" "}
+				<Link to="/posts/create" className="btn">
+					Criar Post
+				</Link>
+			</p>
+		</div>
+	);
+};
 
-export default About
+export default About;
