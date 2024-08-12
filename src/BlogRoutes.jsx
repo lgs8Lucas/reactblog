b84ac23e16280a4cb0e78baa9b3862d112d705ad
Lucas <lgs.08lucas@gmail.com>
2024-08-12 14:53:07 -0300

    Alterando navbar de acordo com autenticação do usuário

--- src/BlogRoutes.jsx
+++ src/BlogRoutes.jsx
@@ -3,18 +3,22 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
-import Login from './pages/Login/Login'
+import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
+import CreatePost from "./pages/CreatePost/CreatePost";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 const BlogRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/about" element={<About />} />
+			<Route path="/login" element={<Login />} />
+			<Route path="/register" element={<Register />} />
+			<Route path="/posts/create" element={<CreatePost />} />
+			<Route path="/dashboard" element={<Dashboard />} />
+		</Routes>
+	);
 };
 
 export default BlogRoutes;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,66 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useAuthentication } from "../hooks/useAuthentication";
+import { useAuthValue } from "../context/AuthContext";
 
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-  return (
-    <nav className={styles.navbar}>
-      <NavLink to={"/"} className={styles.brand}>
-        React<span>Blog</span>
-      </NavLink>
-      <ul className={styles.links_list}>
-        <li>
-          <NavLink className={({isActive}) => isActive? styles.active:''} to="/">Início</NavLink>
-        </li>
-        <li>
-          <NavLink className={({isActive}) => isActive? styles.active:''} to="/login">Entrar</NavLink>
-        </li>
-        <li>
-          <NavLink className={({isActive}) => isActive? styles.active:''} to="/about" >Sobre</NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
+	const { user } = useAuthValue();
+	return (
+		<nav className={styles.navbar}>
+			<NavLink to={"/"} className={styles.brand}>
+				React<span>Blog</span>
+			</NavLink>
+			<ul className={styles.links_list}>
+				<li>
+					<NavLink
+						className={({ isActive }) => (isActive ? styles.active : "")}
+						to="/"
+					>
+						Início
+					</NavLink>
+				</li>
+				{user ? (
+					<>
+						<li>
+							<NavLink
+								className={({ isActive }) => (isActive ? styles.active : "")}
+								to="/posts/create"
+							>
+								Novo Post
+							</NavLink>
+						</li>
+						<li>
+							<NavLink
+								className={({ isActive }) => (isActive ? styles.active : "")}
+								to="/dashboard"
+							>
+								Dashboard
+							</NavLink>
+						</li>
+					</>
+				) : (
+					<li>
+						<NavLink
+							className={({ isActive }) => (isActive ? styles.active : "")}
+							to="/login"
+						>
+							Entrar
+						</NavLink>
+					</li>
+				)}
+				<li>
+					<NavLink
+						className={({ isActive }) => (isActive ? styles.active : "")}
+						to="/about"
+					>
+						Sobre
+					</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
 };
 
 export default Navbar;
