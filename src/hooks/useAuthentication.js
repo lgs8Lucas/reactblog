562067f5e144e9f08d3b3ff62d0d74e2425f20f9
Lucas <lgs.08lucas@gmail.com>
2024-08-12 15:45:11 -0300

    Criado tela e funcionalidade de Login

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -1,68 +1,81 @@
-import {db} from '../firebase/config'
+import { db } from "../firebase/config";
 
 import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  updateProfile,
-  signOut,
+	getAuth,
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+	updateProfile,
+	signOut,
 } from "firebase/auth"; //Pegando funções do Firebase
 
 import { useState, useEffect } from "react";
 
 export const useAuthentication = () => {
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(null);
-  //Cleanup
-  //Limpando memória liquida
-  const [canceled, setCanceled] = useState(false);
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(null);
+	//Cleanup
+	//Limpando memória liquida
+	const [canceled, setCanceled] = useState(false);
 
-  const auth = getAuth();
+	const auth = getAuth();
 
-  function checkIfIsCancelled() {
-    if (canceled) return;
-  }
+	function checkIfIsCancelled() {
+		if (canceled) return;
+	}
 
-  //Register
-  const createUser = async (data) => {
-    checkIfIsCancelled();
-    setLoading(true);
-    try {
-      const { user } = await createUserWithEmailAndPassword(
-        auth,
-        data.email,
-        data.password
-      ); //Cria usuário com login e senha
+	//Register
+	const createUser = async (data) => {
+		checkIfIsCancelled();
+		setLoading(true);
+		try {
+			const { user } = await createUserWithEmailAndPassword(
+				auth,
+				data.email,
+				data.password
+			); //Cria usuário com login e senha
 
-      await updateProfile(user, { displayName: data.displayName }); //Define username do usuário
-      setLoading(false);
-      setError(null);
-      return user;
-    } catch (er) {
-      setLoading(false);
-      console.log(er.message);
-      console.log(typeof er.message);
-      let systemErrorMessage;
-      if (er.message.includes('Password'))
-        systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres.";
-      else if(er.message.includes('email-already'))
-        systemErrorMessage = "Este email já está cadastrado";
-      else
-        systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde"
-      setError(systemErrorMessage)
-    }
-    
-  };
+			await updateProfile(user, { displayName: data.displayName }); //Define username do usuário
+			setLoading(false);
+			setError(null);
+			return user;
+		} catch (er) {
+			let systemErrorMessage;
+			if (er.message.includes("Password"))
+				systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres.";
+			else if (er.message.includes("email-already"))
+				systemErrorMessage = "Este email já está cadastrado";
+			else systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde";
+			setError(systemErrorMessage);
+		}
+		setLoading(false);
+	};
 
-  //Logout / sing out
-  const logout = () =>{
-    checkIfIsCancelled()//Previne memory liq
-    signOut(auth)
-  }
+	//Logout / sign out
+	const logout = () => {
+		checkIfIsCancelled(); //Previne memory liq
+		signOut(auth);
+	};
 
-  useEffect(() => {
-    return () => setCanceled(true);
-  }, []);
+	//Login / sign in
+	const login = async (data) => {
+		checkIfIsCancelled();
+		setLoading(true);
+		setError(false);
+		try {
+			await signInWithEmailAndPassword(auth, data.email, data.password);
+		} catch (er) {
+			let systemErrorMessage;
+			if (er.message.includes("invalid-credential"))
+				systemErrorMessage = "Verifique seu email e senha e tente novamente!";
+			else systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde";
+			setError(systemErrorMessage);
+		}
+		setLoading(false);
+	};
 
-  return { auth, createUser, error, loading, logout };
+	useEffect(() => {
+		return () => setCanceled(true);
+	}, []);
+
+	return { auth, createUser, error, loading, logout, login };
 };
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,71 @@
-import React from 'react'
-import styles from './Login.module.css'
+import React from "react";
+import styles from "./Login.module.css";
+
+import { useState, useEffect } from "react";
+import { useAuthentication } from "../../hooks/useAuthentication";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState("");
+	const { error: authError, loading, login } = useAuthentication();
 
-export default Login
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const user = {
+			email,
+			password,
+		};
+		setError("");
+		const res = await login(user);
+	};
+
+	useEffect(() => {
+		if (authError) {
+			setError(authError);
+		} else {
+			setError("");
+		}
+	});
+
+	return (
+		<div className={styles.login}>
+			<h1>Entrar</h1>
+			<p>Faça o login para começar a navegar pelos posts!</p>
+			<form onSubmit={handleSubmit}>
+				<label>
+					<span>Email:</span>
+					<input
+						type="email"
+						name="email"
+						required
+						placeholder="Insira seu Email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</label>
+				<label>
+					<span>Senha:</span>
+					<input
+						type="password"
+						name="password"
+						required
+						placeholder="Insira sua senha"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</label>
+				<button type="submit" className="btn" disabled={loading}>
+					{loading ? "Aguarde" : "Logar"}
+				</button>
+				{error && <p className="error">{error}</p>}
+			</form>
+			<p>
+				Ainda não tem um login? <Link to="/register">Cadastre-se ja!</Link>
+			</p>
+		</div>
+	);
+};
+
+export default Login;
